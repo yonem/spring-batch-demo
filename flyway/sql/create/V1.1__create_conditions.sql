0efde33d12f6bbc5c7fc9696394275b9c3dd0d97
yonem <yonem321@gmail.com>
2024-01-16 02:45:35 +0900

    feat: 各DBにテーブルを追加する #13

--- flyway/sql/create/V1.1__create_conditions.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE `conditions` (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `name` varchar(40) NOT NULL COMMENT 'ラベル名',
-  `condison` varchar(4000) COMMENT '検索条件',
-  PRIMARY KEY (`id`)
-) COMMENT='検索条件';
--- flyway/sql/main/create/V1.1.1__create_m_workers.sql
+++ flyway/sql/main/create/V1.1.1__create_m_workers.sql
@@ -1,9 +1,8 @@
-﻿CREATE TABLE `books` (
+﻿CREATE TABLE `m_workers` (
   `id` int NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `name` varchar(40) NOT NULL COMMENT '書籍名',
-  `isbn` varchar(255) NOT NULL COMMENT 'ISBN',
+  `name` varchar(50) NOT NULL COMMENT '作業者名',
+  `is_valid` int NOT NULL COMMENT '有効フラグ',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '登録日',
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新日付',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `isbn` (`isbn`)
-) COMMENT='書籍';
+  PRIMARY KEY (`id`)
+) COMMENT='作業者';
