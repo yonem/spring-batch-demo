fa50ec663e800120c477cc97884b2838a7d8f654
yonem <yonem321@gmail.com>
2024-01-22 01:09:48 +0900

    feat: テストデータを追加する #15

--- flyway/sql/insert/V2.1__insert_conditions.sql
+++
@@ -1,7 +0,0 @@
-﻿INSERT INTO conditions VALUES
-(1, 'name-1', 'WHERE id IN(1, 3)'),
-(2, 'name-2', 'WHERE id IN(1, 2)'),
-(3, 'name-3', 'WHERE id IN(1, 3, 4)'),
-(4, 'name-4', 'WHERE id IN(5)'),
-(5, 'name-5', 'WHERE id IN(4, 6)'),
-(6, 'name-6', 'WHERE id IN(1, 4)');
