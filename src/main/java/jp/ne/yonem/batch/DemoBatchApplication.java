690c8e730a2e616bcf3e6faf5fe063abf2ab7c35
yonem <yonem321@gmail.com>
2024-01-25 00:33:28 +0900

    feat: DataSourceを複数構成にする #21

--- src/main/java/jp/ne/yonem/batch/DemoBatchApplication.java
+++ src/main/java/jp/ne/yonem/batch/DemoBatchApplication.java
@@ -1,12 +1,17 @@
 package jp.ne.yonem.batch;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("jp.ne.yonem.**.mapper")
 public class DemoBatchApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoBatchApplication.class, args);
-	}
+    public static void main(String[] args) {
+        var application = new SpringApplication(DemoBatchApplication.class);
+        application.setWebApplicationType(WebApplicationType.NONE);
+        application.run();
+    }
 }
